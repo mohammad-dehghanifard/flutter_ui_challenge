0dc9b7411caeca95dd8f032612d0b84b1bcedf1b
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-14 22:43:20 -0800

    game details ui : impl FavoriteBtn custom widget

--- lib/game_detail/screen/game_detail_screen.dart
+++ lib/game_detail/screen/game_detail_screen.dart
@@ -1,8 +1,11 @@
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenge/game_detail/data/fake_game_data.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../widgets/back_btn.dart';
 import '../widgets/detail_app_bar.dart';
+import '../widgets/favorite_btn.dart';
 
 class GameDetailScreen extends StatelessWidget {
   const GameDetailScreen({super.key});
@@ -17,6 +20,7 @@ class GameDetailScreen extends StatelessWidget {
       ),
       body:  Column(
         children: [
+          const SizedBox(height: 8),
           // slider
          CarouselSlider.builder(
              itemCount: FakeGameData.sliders.length,
@@ -28,7 +32,56 @@ class GameDetailScreen extends StatelessWidget {
                     borderRadius: BorderRadius.circular(12),
                      child: Image.asset(FakeGameData.sliders[index],fit: BoxFit.fill,))
              ),
-             options: CarouselOptions(autoPlay: true))
+             options: CarouselOptions(autoPlay: true)),
+          const SizedBox(height: 24),
+          // game detail
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(12),
+              decoration: const BoxDecoration(
+                  color: CupertinoColors.tertiarySystemBackground,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(32),
+                    topLeft: Radius.circular(32),
+                  )),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  // name and favorite btn
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      //name
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          //game info
+                          Text(
+                            "call of duty modern warfare 3",
+                            style: GoogleFonts.alata(
+                                color: CupertinoColors.black,
+                                fontWeight: FontWeight.w700,
+                                fontSize: 18
+                            ),),
+                          Text(
+                            "action - shooter",
+                            style: GoogleFonts.alata(
+                                color: CupertinoColors.inactiveGray,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 14
+                            ),),
+                        ],
+                      ),
+                      // favorite btn
+                      FavoriteBtn(onTap: () {}),
+                      const SizedBox()
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
---
+++ lib/game_detail/widgets/favorite_btn.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/cupertino.dart';
+
+class FavoriteBtn extends StatelessWidget {
+  const FavoriteBtn({
+    super.key, required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 50,
+        height: 50,
+        decoration: const BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+                colors: [
+                  Color(0xFFF3A470),
+                  Color(0xFFF88A42),
+                ]
+            )
+        ),
+        child: const Center(child: Icon(CupertinoIcons.suit_heart_fill,color: CupertinoColors.white,size: 36,)),
+      ),
+    );
+  }
+}
