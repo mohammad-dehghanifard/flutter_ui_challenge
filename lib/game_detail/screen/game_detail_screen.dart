a97fb9575069d3f8ca37a73bbe3116b3efd25122
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2023-11-14 21:33:53 -0800

    upload project

---
+++ lib/game_detail/screen/game_detail_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GameDetailScreen extends StatelessWidget {
+  const GameDetailScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ui_challenge/game_detail/screen/game_detail_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: false,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const GameDetailScreen(),
+    );
+  }
+}
