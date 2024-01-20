c28c0ac44d38b97832462ba9d11d81be382c7aaf
mohammad dehghanifard <104780008+mohammad-dehghanifard@users.noreply.github.com>
2024-01-20 08:00:11 -0800

    doctor app : impl DoctorCategory class

---
+++ lib/doctor_app/util/doctor_category.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class DoctorCategory {
+  DoctorCategory({required this.iconPath,required this.colors});
+  final List<Colors> colors;
+  final String iconPath;
+}
